245231fb2b72f9d9976bdfe52d2f1fd57a33aa45
Xulio Xaviert Rojas Teixeira <xulio.rojas@vasscompany.com>
2024-11-04 11:37:28 +0100

    feat: multi-language service created

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,20 +1,45 @@
-import { ApplicationConfig } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
-import { HTTP_INTERCEPTORS, provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http';
+import {
+  HTTP_INTERCEPTORS,
+  provideHttpClient,
+  withFetch,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 import { routes } from './app.routes';
 import { HttpInterceptorService } from './core/interceptors/http-interceptor.service';
 
+export function HttpLoaderFactory(http: HttpClient): TranslateLoader {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 export const appConfig: ApplicationConfig = {
-  providers: [ provideRouter(routes),
+  providers: [
+    provideRouter(routes),
     provideHttpClient(withInterceptorsFromDi()),
     {
       provide: HTTP_INTERCEPTORS,
       useClass: HttpInterceptorService,
       multi: true,
     },
-  provideHttpClient(withFetch()),
-
-
-  ]
+    provideHttpClient(),
+    provideHttpClient(withFetch()),
+    {
+      provide: TranslateLoader,
+      useFactory: HttpLoaderFactory,
+      deps: [ HttpClient ]
+    },
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [ HttpClient ]
+        }
+      })
+    )
+  ],
 };

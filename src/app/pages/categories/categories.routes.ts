439a9f68e76c7576ef1cac2fc86cfea06f6bf20a
Xulio Xaviert Rojas Teixeira <xulio.rojas@vasscompany.com>
2024-12-19 13:55:35 +0100

    feat: created feature.component

--- src/app/pages/categories/categories.routes.ts
+++ src/app/pages/categories/categories.routes.ts
@@ -16,23 +16,43 @@ export const routes: Routes = [
     path: '',
     component: CategoriesComponent,
     children: [
+      // {
+      //   path: 'electronics',
+      //   component: ElectronicsComponent
+
+      // },
+      // {
+      //   path: 'jewelry',
+      //   component: JewelryComponent
+      // },
+      // {
+      //   path: 'men',
+      //   component: MenComponent
+      // },
+      // {
+      //   path: 'women',
+      //   component: WomenComponent
+      // },
       {
         path: 'electronics',
-        component: ElectronicsComponent
-
+        loadComponent: () => import('./electronics/electronics.component').then(m => m.ElectronicsComponent),
       },
       {
         path: 'jewelry',
-        component: JewelryComponent
+        loadComponent: () => import('./jewelry/jewelry.component').then(m => m.JewelryComponent)
       },
       {
         path: 'men',
-        component: MenComponent
+        loadComponent: () => import('./men/men.component').then(m => m.MenComponent)
       },
       {
         path: 'women',
-        component: WomenComponent
+        loadComponent: () => import('./women/women.component').then(m => m.WomenComponent)
       },
+      {
+        path: 'feature',
+        loadComponent: () => import('./feature/feature.component').then(m => m.FeatureComponent)
+      }
     ],
   }
 

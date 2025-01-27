548b67067079482aeb89544fd970d6d257c21344
Xulio Xaviert Rojas Teixeira <xulio.rojas@vasscompany.com>
2025-01-27 11:50:29 +0100

    feat: updated cart.component

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,7 +1,6 @@
 import { DecimalPipe, NgFor, UpperCasePipe } from '@angular/common';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { map, switchMap } from 'rxjs';
 import { AuthService } from '../../auth/auth.service';
 import { ShoppingCart } from '../../core/models/cart.model';
 import { Products } from '../../core/models/products.model';
@@ -17,13 +16,14 @@ import { UsersService } from '../../users/users.service';
   styleUrl: './cart.component.scss',
 })
 export class CartComponent implements OnInit, OnDestroy {
-  shoppingCart: any[] = [];
+  shoppingCart: ShoppingCart = {} as ShoppingCart;
   total: number = 0;
   shipping: number = 4;
   tax: number = 0;
   subTotal: number = 0;
   cartProductTotal: number = 0;
   user: Users;
+
 
   constructor(
     private usersService: UsersService,
@@ -34,59 +34,35 @@ export class CartComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.getData();
+    this.getIdFromUrl()
+  }
+
+  getIdFromUrl(): string {
+    const url = this.router.url;
+    const cartId = url.split('/');
+    return cartId[ cartId.length - 1 ];
   }
 
   getData(): void {
     if (this.authService.isAuthenticated()) {
-      this.user = this.authService.getSessionStorage('user');
       this.usersService
-        .getShoppingCartByUserId(this.user.userId)
-        .pipe(
-          switchMap((shoppingCart: any) =>
-            this.httpService.getAllProducts().pipe(
-              map((products: any) => ({
-                shoppingCart: shoppingCart[ 0 ],
-                products,
-              }))
-            )
-          )
-        )
-        .subscribe(({ shoppingCart, products }: any) => {
-          this.shoppingCartCalculation(shoppingCart, products);
-
+        .getShoppingCartById(this.getIdFromUrl())
+        .subscribe((shoppingCart: ShoppingCart) => {
+          console.log(".subscribe / shoppingCart:", shoppingCart);
+          this.shoppingCart = shoppingCart;
+          this.shoppingCartCalculation(shoppingCart);
         });
     } else {
       this.router.navigate([ '/login' ]);
     }
   }
 
-  shoppingCartCalculation(shoppingCart: any, products: any) {
+  shoppingCartCalculation(shoppingCart: any) {
 
-    let oneSize = false;
     let subTotal = 0;
     let cartProductTotal = 0;
-    shoppingCart.products.forEach((product: any) => {
-      const productData = products.find(
-        (p: any) => p.productId === product.productId
 
-      );
-      product.quantity.forEach((element: any) => {
-        subTotal += productData.price * element.quantity;
-        cartProductTotal += element.quantity;
-        if (element.size === 'U') {
-          oneSize = true;
-        }
-      });
-      //TODO: Ojo pendioente de todas las propiedades
-      this.shoppingCart.push({
-        ...productData,
-        ...product,
-        subTotal,
-        cartProductTotal,
-        oneSize,
-      });
-      this.subTotal += subTotal;
-    });
+
 
     this.tax = this.subTotal * 0.21;
     this.total = this.subTotal + this.tax + this.shipping;
@@ -109,45 +85,45 @@ export class CartComponent implements OnInit, OnDestroy {
 
   decrementQuantity(cart: any, size: any): void {
     console.log('decrementQuantity', cart, size);
-    this.shoppingCart = this.shoppingCart.map(product => {
-      if (product.productId === cart.productId) {
-        product.size = product.quantity.map((s: any) => {
-          if (s.size === size.size) {
-            if (s.quantity <= 0) return s;
+    // this.shoppingCart = this.shoppingCart.products.map(product => {
+    //   if (product.productId === cart.productId) {
+    //     product.size = product.quantity.map((s: any) => {
+    //       if (s.size === size.size) {
+    //         if (s.quantity <= 0) return s;
 
-            s.quantity -= 1;
-            product.subTotal -= product.price;
-          }
-          return s;
-        });
-        product.cartProductTotal = product.size.reduce((total: number, s: any) => total += s.quantity, 0);
+    //         s.quantity -= 1;
+    //         product.subTotal -= product.price;
+    //       }
+    //       return s;
+    //     });
+    //     product.cartProductTotal = product.size.reduce((total: number, s: any) => total += s.quantity, 0);
 
-      }
-      return product;
-    });
-    this.subTotal = this.shoppingCart.reduce((total: number, product: any) => total += product.subTotal, 0);
-    this.tax = this.subTotal * 0.21 + this.shipping * 0.21;
-    this.total = this.subTotal + this.tax + this.shipping;
+    //   }
+    //   return product;
+    // });
+    // this.subTotal = this.shoppingCart.reduce((total: number, product: any) => total += product.subTotal, 0);
+    // this.tax = this.subTotal * 0.21 + this.shipping * 0.21;
+    // this.total = this.subTotal + this.tax + this.shipping;
   }
 
   incrementQuantity(productId: number, size: any): void {
     console.log('incrementQuantity', productId, size);
-    this.shoppingCart = this.shoppingCart.map(product => {
-      if (product.productId === productId) {
-        product.size = product.quantity.map((s: any) => {
-          if (s.size === size.size) {
-            s.quantity += 1;
-            product.subTotal += product.price;
-          }
-          return s;
-        });
-        product.cartProductTotal = product.size.reduce((total: number, s: any) => total += s.quantity, 0);
-      }
-      return product;
-    });
-    this.subTotal = this.shoppingCart.reduce((total: number, product: any) => total += product.subTotal, 0);
-    this.tax = this.subTotal * 0.21 + this.shipping * 0.21;
-    this.total = this.subTotal + this.tax + this.shipping;
+    // this.shoppingCart = this.shoppingCart.map(product => {
+    //   if (product.productId === productId) {
+    //     product.size = product.quantity.map((s: any) => {
+    //       if (s.size === size.size) {
+    //         s.quantity += 1;
+    //         product.subTotal += product.price;
+    //       }
+    //       return s;
+    //     });
+    //     product.cartProductTotal = product.size.reduce((total: number, s: any) => total += s.quantity, 0);
+    //   }
+    //   return product;
+    // });
+    // this.subTotal = this.shoppingCart.reduce((total: number, product: any) => total += product.subTotal, 0);
+    // this.tax = this.subTotal * 0.21 + this.shipping * 0.21;
+    // this.total = this.subTotal + this.tax + this.shipping;
   }
 
   makePayment(): void {
@@ -155,18 +131,18 @@ export class CartComponent implements OnInit, OnDestroy {
       const user = this.authService.getSessionStorage('user');
       console.log("makePayment / user:", this.shoppingCart);
       let newShoppingCart: ShoppingCart = {} as ShoppingCart;
-      this.usersService.getShoppingCartByUserId(this.user.userId).subscribe((shoppingCart: any) => {
-        newShoppingCart = shoppingCart[ 0 ];
-        newShoppingCart.products = [
-          ...this.shoppingCart,
-        ]
+      // this.usersService.getShoppingCartByUserId(this.user.userId).subscribe((shoppingCart: any) => {
+      //   newShoppingCart = shoppingCart[ 0 ];
+      //   newShoppingCart.products = [
+      //     ...this.shoppingCart,
+      //   ]
 
-        console.log("makePayment / newShoppingCart:", newShoppingCart);
-        newShoppingCart.products.forEach((product: any) => {
-          product.quantity = 0;
-          delete product.quantity
-        })
-      })
+      //   console.log("makePayment / newShoppingCart:", newShoppingCart);
+      //   newShoppingCart.products.forEach((product: any) => {
+      //     product.quantity = 0;
+      //     delete product.quantity
+      //   })
+      // })
 
     }
   }

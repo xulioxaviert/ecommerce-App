252c4b6e4c8027437e3ee20f07f4902f1482e16a
Xulio Xaviert Rojas Teixeira <xulio.rojas@vasscompany.com>
2025-01-29 12:35:20 +0100

    feat: created cart-lis and oder-summary components

---
+++ src/app/pages/cart/cart-list/cart-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-list',
+  imports: [],
+  standalone: true,
+  templateUrl: './cart-list.component.html',
+  styleUrl: './cart-list.component.scss'
+})
+export class CartListComponent {
+
+}
---
+++ src/app/pages/cart/order-summary/order-summary.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-summary',
+  imports: [],
+  standalone: true,
+  templateUrl: './order-summary.component.html',
+  styleUrl: './order-summary.component.scss'
+})
+export class OrderSummaryComponent {
+
+}

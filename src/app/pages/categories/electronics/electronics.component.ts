9b9ed93479f0969e34302eb9efc37fdc1c587f45
Xulio Xaviert Rojas Teixeira <xulio.rojas@vasscompany.com>
2025-02-17 11:20:40 +0100

    feat: refactor code

--- src/app/pages/categories/electronics/electronics.component.ts
+++ src/app/pages/categories/electronics/electronics.component.ts
@@ -1,18 +1,13 @@
 import { CommonModule, NgForOf, NgIf } from '@angular/common';
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
-import { ConfirmationService } from 'primeng/api';
-import { Subscription } from 'rxjs';
-import { AuthService } from '../../../auth/auth.service';
 import { ENDPOINTS } from '../../../core/const/constants';
 import { Product, ShoppingCart } from '../../../core/models/cart.model';
 import { Users } from '../../../core/models/user.model';
 import { HttpService } from '../../../core/services/http.service';
-import { ToastService } from '../../../core/services/toast.service';
 import { ModalService } from '../../../shared/modal/product-modal-service.service';
 import { ProductModal } from '../../../shared/modal/product-modal.component';
-import { UsersService } from '../../../users/users.service';
 
 @Component({
   selector: 'app-mens',
@@ -22,30 +17,22 @@ import { UsersService } from '../../../users/users.service';
   styleUrl: './electronics.component.scss',
   providers: [],
 })
-export class ElectronicsComponent implements OnInit, OnDestroy {
+export class ElectronicsComponent implements OnInit {
   products: Product[] = [];
   currentProduct: Product = {} as Product;
   visible: boolean = false;
-  subscription: Subscription = new Subscription();
   user: Users = {} as Users;
   shoppingCart: ShoppingCart = {} as ShoppingCart;
 
   constructor(
     private http: HttpService,
     private router: Router,
-    private modalService: ModalService,
-    private authService: AuthService,
-    private usersService: UsersService,
-    private toastService: ToastService,
-    private confirmationService: ConfirmationService
+    private modalService: ModalService
+
   ) { }
 
   ngOnInit(): void {
     this.getData();
-    this.getSubscriptions();
-  }
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
   }
 
   getData() {
@@ -55,18 +42,6 @@ export class ElectronicsComponent implements OnInit, OnDestroy {
         .forEach((product: Product) => this.products.push(product));
     });
 
-  }
-
-
-  getSubscriptions() {
-    this.subscription.add(
-      this.authService.isAuthenticated$.subscribe((isAuthenticated) => {
-
-      })
-    );
-    this.subscription.add(
-
-    );
   }
 
   navigateToProductDetail(product: Product) {
